970d2ce82c56a6bbf75163674ac1d2dd5b794d98
Peter Young <young40@gmail.com>
2017-06-24 22:57:04 +0800

    增加场景跳转

---
+++ Assets/Script/Login.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour {
+	public Button btnLogin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Awake() {
+		btnLogin.onClick.AddListener (onPressLogin);
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onPressLogin() {
+		SceneManager.LoadScene ("_SceneMain");
+	}
+}
